dade205a00562ce3b3b20fe81dd1afdb17569b46
Bombiz <kombizkhayami@outlook.com>
2017-09-17 10:43:45 -0400

    updated api call.
    blizzard updated their api calls so they now require a key to access. updated api call to include the key

--- PHP/Diablo 3 project/kk/hero.php
+++ PHP/Diablo 3 project/kk/hero.php
@@ -10,8 +10,9 @@ $smarty = Smart();
 
 if( isset($_SESSION['battleTag']) )
 {
+	$api_key="6mxq9g3c5ycpzg3se5xa29eb967j4edj";
 	$battle_tag = $_SESSION['battleTag'];
-        $file=file_get_contents("http://us.battle.net/api/d3/profile/$battle_tag/");
+	$file=file_get_contents("https://us.api.battle.net/d3/profile/$battle_tag/?apikey=$api_key/");
 
 	$json=json_decode($file, true);
 
